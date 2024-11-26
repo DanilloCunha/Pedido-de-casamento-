6d35e139e9db7cb0eaf6be8ec6b812d0c3a228b2
Danillo Cunha <107063327+DanilloCunha@users.noreply.github.com>
2024-11-26 20:19:35 -0300

    formulario para casamento
     KKKK

---
+++ c.rb
@@ -0,0 +1,16 @@
+# Pergunta o nome da pessoa
+print "Qual é o seu nome? "
+nome = gets.chomp
+
+# Faz a pergunta
+puts "Olá, #{nome}, você aceita casar comigo? (sim/não)"
+resposta = gets.chomp.downcase
+
+# Verifica a resposta e exibe a reação
+if resposta == "sim"
+  puts "😀 Que alegria, #{nome}! Estou muito feliz!"
+elsif resposta == "não"
+  puts "😢 Poxa, #{nome}, tudo bem... Vou sobreviver!"
+else
+  puts "🤔 Não entendi sua resposta, #{nome}."
+end
